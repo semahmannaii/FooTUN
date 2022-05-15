86d1ea7ec8a0a389da2aa0a57037a0c118d01e41
Semah Mannaii <74506986+semahmannaii@users.noreply.github.com>
2022-05-15 09:53:00 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
                     <div>
                         <h1 className="text-success text-uppercase fs-1 fw-bold">Greatest Football Store</h1>
                         <p className="mt-4 text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean finibus ligula non porta dictum. Nullam in ante commodo, interdum dui sit amet, eleifend turpis. Nulla tincidunt mauris est, non rhoncus ante aliquet posuere. Vestibulum sed sem nibh. Vestibulum tempor velit sem, in laoreet lacus facilisis vitae. Mauris feugiat tincidunt erat non mattis. Aliquam facilisis, mi sed finibus rutrum, est eros pretium velit, eget interdum risus enim eget mauris.</p>
-                        <Link to="products" className="btn btn-success px-3 mt-3">Shop Now</Link>
+                        <Link to="products" className="btn btn-success px-3 my-3">Shop Now</Link>
                     </div>
                 </div>
             </div>
